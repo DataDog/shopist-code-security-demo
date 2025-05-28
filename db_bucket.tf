79dc19b1e2ee8aae71e2b7a72392d2205aebf64f
Bahar Shah <bahar.shah@datadoghq.com>
2025-05-28 12:03:16 -0400

    Update db_bucket.tf

--- db_bucket.tf
+++ db_bucket.tf
@@ -1,122 +1,31 @@
-resource "google_sql_database_instance" "sql_database_instance" {
-  name             = var.name
-  database_version = var.database_version
-  region           = var.region
-  settings {
-    tier = var.tier
-
-    dynamic "backup_configuration" {
-      for_each = var.backup_enabled != null || var.backup_start_time != null ? [1] : []
-      content {
-        enabled    = var.backup_enabled
-        start_time = var.backup_start_time
-      }
-    }
-
-    user_labels = var.user_labels
-  }
-}
-
-resource "google_storage_bucket" "bucket" {
-  name     = var.cloud_storage_bucket_name
-  location = var.region
-  project  = var.project_name
-}
-
-
-resource "aws_s3_bucket" "remediation_demo_bucket" {
-  bucket = "iac-remediation-bucket"
+resource "aws_db_instance" "shopist_mysql_db_dev" {
+  identifier          = "shopist-mysql-db"
+  engine              = "mysql"
+  instance_class      = "db.t2.micro"
+  allocated_storage   = 10
+  username            = "admin"
+  password            = "password"
+  publicly_accessible = false
 
   tags = {
-    "dd_git_file"           = "terraform/main.tf"
+    "dd_git_file"           = "terraform/db.tf"
     "dd_git_org"            = "DataDog"
     "dd_git_repo"           = "github.com/DataDog/shopist-code-security-demo"
-    "dd_git_resource_lines" = "1:9"
+    "dd_git_resource_lines" = "1:17"
+    "dd_resource_signature" = "resource.aws_db_instance.shopist_mysql_db"
   }
 }
 
-resource "aws_s3_bucket" "remediation_demo_bucket" {
-  bucket = "iac-remediation-bucket"
+resource "google_sql_database_instance" "bad_example" {
+  name             = "bad-instance"
+  database_version = "MYSQL_8"
+  region           = "us-central1"
 
-  tags = {
-    "dd_git_file"           = "terraform/main.tf"
-    "dd_git_org"            = "DataDog"
-    "dd_git_repo"           = "github.com/DataDog/shopist-code-security-demo"
-    "dd_git_resource_lines" = "1:9"
-  }
-}
-
-resource "aws_s3_bucket_acl" "remediation_demo_bucket" {
-  bucket = aws_s3_bucket.remediation_demo_bucket.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_versioning" "versioning_remediation_demo_bucket" {
-  bucket = aws_s3_bucket.remediation_demo_bucket.id
-  versioning_configuration {
-    status = "Disabled"
-  }
-}
-
-resource "aws_s3_bucket_acl" "remediation_demo_bucket" {
-  bucket = aws_s3_bucket.remediation_demo_bucket.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_versioning" "versioning_remediation_demo_bucket" {
-  bucket = aws_s3_bucket.remediation_demo_bucket.id
-  versioning_configuration {
-    status = "Disabled"
-  }
-}
-
-data "aws_availability_zones" "available_iac" {
-  state = "available"
-}
-
-resource "aws_ebs_volume" "iac_volume" {
-  availability_zone = data.aws_availability_zones.available_iac.names[0]
-  size              = 1
-
-  tags = {
-    Name = "iac-scanning"
-    Team = "demo"
-  }
-}
-
-resource "aws_s3_bucket_acl" "remediation_demo_bucket" {
-  bucket = aws_s3_bucket.remediation_demo_bucket.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_versioning" "versioning_remediation_demo_bucket" {
-  bucket = aws_s3_bucket.remediation_demo_bucket.id
-  versioning_configuration {
-    status = "Disabled"
-  }
-}
-
-resource "google_sql_database_instance" "sql_database_instance" {
-  name             = var.name
-  database_version = var.database_version
-  region           = var.region
   settings {
-    tier = var.tier
-
-    dynamic "backup_configuration" {
-      for_each = var.backup_enabled != null || var.backup_start_time != null ? [1] : []
-      content {
-        enabled    = var.backup_enabled
-        start_time = var.backup_start_time
-      }
+    tier = "db-custom-2-13312"
+    database_flags {
+      name  = "cross db ownership chaining"
+      value = "on"
     }
-
-    user_labels = var.user_labels
   }
-}
-
-resource "google_storage_bucket" "bucket" {
-  name     = var.cloud_storage_bucket_name
-  location = var.region
-  project  = var.project_name
 }
