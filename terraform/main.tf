8b5f6d22d32a7132f37a8786a7428e6c8e362970
Bahar Shah <bahar.shah@datadoghq.com>
2025-05-28 09:45:10 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,7 +21,28 @@ resource "aws_s3_bucket_versioning" "versioning_remediation_demo_bucket" {
   }
 }
 
-resource "aws_s3_bucket_acl" "remediation_demo_bucket_2" {
-  bucket = aws_s3_bucket.remediation_demo_bucket_2.id
+resource "aws_s3_bucket_acl" "remediation_demo_bucket" {
+  bucket = aws_s3_bucket.remediation_demo_bucket.id
   acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "versioning_remediation_demo_bucket" {
+  bucket = aws_s3_bucket.remediation_demo_bucket.id
+  versioning_configuration {
+    status = "Disabled"
+  }
+}
+
+data "aws_availability_zones" "available_iac" {
+  state = "available"
+}
+
+resource "aws_ebs_volume" "iac_volume" {
+  availability_zone = data.aws_availability_zones.available_iac.names[0]
+  size              = 1
+
+  tags = {
+    Name = "iac-scanning"
+    Team = "demo"
+  }
 }
