6da84eca78dbc68aba98047138068fbc023ffa43
NathanGallet-dd <nathan.gallet@datadoghq.com>
2026-09-01 19:12:25 +0200

    Update terraform/termination.tf
    Co-authored-by: datadog-datadog-demo-org[bot] <156791763+datadog-datadog-demo-org[bot]@users.noreply.github.com>

--- terraform/termination.tf
+++ terraform/termination.tf
@@ -1,6 +1,10 @@
 resource "aws_instance" "example" {
   ami = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
+  
+  metadata_options {
+    http_tokens = "required"
+  }
   disable_api_termination = false
   tags = {
     Name = "test-instance"
