d993c371a67d7a311c4c42d2091dc9041ac4af4e
datadog-bits <263423550+datadog-bits@users.noreply.github.com>
2026-09-07 09:04:32 +0000

    Enable EC2 termination protection

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "example" {
   ami = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
-  disable_api_termination = false
+  disable_api_termination = true
 
   metadata_options {
     http_tokens = "required"
